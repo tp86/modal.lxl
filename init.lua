f463c241a12c8f66c1c5c698c3759aa44f9f25fe
tp86 <tom0palka+tp86@gmail.com>
2023-08-13 13:48:24 +0200

    Modal submap implementation

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- mod-version:3
 local core = require "core"
 local keymap = require "core.keymap"
+local command = require "core.command"
 
 local thismodule = (...)
 local originalkeymap = require(thismodule ..  ".keymap")
@@ -24,14 +25,16 @@ local function setupfallback(mt, fallbackkeys)
       return keys[key]
     end
   end
+  local index = mt.__index
   mt.__index = function(_, key)
     if fallback(key) then
       return originalkeymap.map[key]
     end
-    core.log(key .. " not mapped")
-    return { nop }
+    return index(_, key)
   end
 end
+
+local reset
 
 local function restore(maponly)
   keymap.map = originalkeymap.map
@@ -51,44 +54,95 @@ end
 
 local modal = {}
 
+local function makemode(modekey, modemap, mt, wrapper)
+  local mode = {}
+  local keys = {}
+  for key, actions in pairs(modemap) do
+    if key == "fallback" then
+      setupfallback(mt, actions)
+    elseif key == "onenter" then
+      mode.onenter = actions
+    else
+      keys[key] = wrapper(actions)
+    end
+  end
+  local map, reversemap = createmaps(keys, mt)
+  mode.map = map
+  mode.reversemap = reversemap
+  modes[modekey] = mode
+end
+
+local mapmt = {
+  __index = function(_, key)
+    core.log(key .. " not mapped")
+    return { nop }
+  end
+}
+
+local function idwrap(actions) return actions end
+
 function modal.map(modemaps)
   for name, modemap in pairs(modemaps) do
-    local mode = {}
-    local keys, mt = {}, {}
-    for key, actions in pairs(modemap) do
-      if key == "fallback" then
-        setupfallback(mt, actions)
-      elseif key == "onenter" then
-        mode.onenter = actions
-      else
-        keys[key] = actions
-      end
-    end
-    local map, reversemap = createmaps(keys, mt)
-    mode.map = map
-    mode.reversemap = reversemap
-    modes[name] = mode
+    makemode(name, modemap, mapmt, idwrap)
   end
+end
+
+local function setmode(modekey)
+  local mode = modes[modekey]
+  keymap.map = mode.map
+  keymap.reverse_map = mode.reversemap
+  if type(mode.onenter) == "function" then
+    mode.onenter()
+  end
+end
+
+local function wrap(actions)
+  local wrapped = {}
+  if type(actions) ~= "table" then
+    actions = { actions }
+  end
+  for _, action in ipairs(actions) do
+    local newaction = function(...)
+      local performed = false
+      if type(action) == "string" then
+        performed = command.perform(action, ...)
+      elseif type(action) == "function" then
+        performed = action(...)
+      end
+      reset()
+      return performed
+    end
+    table.insert(wrapped, newaction)
+  end
+  table.insert(wrapped, reset)
+  return wrapped
+end
+
+local submapmt = {
+  __index = function()
+    return { reset }
+  end
+}
+
+function modal.submap(map)
+  local function activatesubmode()
+    setmode(activatesubmode)
+  end
+
+  makemode(activatesubmode, map, submapmt, wrap)
+
+  return activatesubmode
 end
 
 function modal.mode(modename)
   return function()
-    local mode = modes[modename]
-    --core.log("activating mode %s", modename)
-    keymap.map = mode.map
-    keymap.reverse_map = mode.reversemap
-    if type(mode.onenter) == "function" then
-      mode.onenter()
-    end
+    setmode(modename)
   end
 end
 
 function modal.activate(modename)
-  local modefn = modal.mode(modename)
-  viewmodifier.activate(modefn, restore)
-  -- modefn()
-  -- keymap.add = nop
-  -- keymap.add_direct = nop
+  reset = modal.mode(modename)
+  viewmodifier.activate(reset, restore)
 end
 
 return modal
